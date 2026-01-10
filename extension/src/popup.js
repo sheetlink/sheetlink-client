4cb972394bd490b3a48b8261d7b2dbe75f07b297
rudymdc <r.delcampo13@gmail.com>
2026-01-10 03:44:57 -0600

    refactor: simplify PRO tier time estimates to '10+ sec'
    Changed time estimates from ranges to simpler '10+ sec' format:
    - First sync: '~10-30 sec' → '10+ sec'
    - Regular sync: '10-20 sec' → '10+ sec'
    
    Benefits:
    - Simpler messaging (less verbose)
    - More accurate (accounts for occasional longer syncs)
    - Sets minimum expectation without upper bound

--- extension/src/popup.js
+++ extension/src/popup.js
@@ -1657,9 +1657,9 @@ async function handleSyncNow() {
 
           // PRO tier: Always show time estimate since syncs can be slow
           if (isProTier && isFirstSync) {
-            loadingMsg = `Syncing ${institution.institutionName}... (Loading 2 years, ~10-30 sec)`;
+            loadingMsg = `Syncing ${institution.institutionName}... (Loading 2 years, 10+ sec)`;
           } else if (isProTier) {
-            loadingMsg = `Syncing ${institution.institutionName}... (This may take 10-20 sec)`;
+            loadingMsg = `Syncing ${institution.institutionName}... (This may take 10+ sec)`;
           } else if (isFirstSync) {
             loadingMsg = `Syncing ${institution.institutionName}... (Keep extension open)`;
           }
